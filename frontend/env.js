db40ff34d9741a8b4651515acd9c5d8c8552a1e1
zhenzhou66 <zhenzhou0609@gmail.com>
2025-03-07 20:31:03 +0800

    classes page started

--- frontend/env.js
+++ frontend/env.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://192.168.164.225:3000";
+const API_BASE_URL = "http://192.168.0.18:3000";
 
 export default API_BASE_URL;
--- frontend/navigation/HomeStack.js
+++ frontend/navigation/HomeStack.js
@@ -5,21 +5,26 @@ import MemberDashboard from "../screens/Home/MemberDashboard";
 import ProfileStack from "./ProfileStack";
 import Notification from "../screens/Home/Notification";
 import CheckIn from "../screens/Home/CheckIn";
+import Classes from "../screens/Home/ClassesPage/Classes";
 
 const HomeStackNavigator = createStackNavigator();
 
 const HomeStack = () => {
-    return (
-        <HomeStackNavigator.Navigator
-            initialRouteName="MemberDashboard"
-            screenOptions={{ headerShown: false }}
-        >
-            <HomeStackNavigator.Screen name="MemberDashboard" component={MemberDashboard} />
-            <HomeStackNavigator.Screen name="Notification" component={Notification} />
-            <HomeStackNavigator.Screen name="ProfileStack" component={ProfileStack} />
-            <HomeStackNavigator.Screen name="CheckIn" component={CheckIn} />
-        </HomeStackNavigator.Navigator>
-    );
+  return (
+    <HomeStackNavigator.Navigator
+      initialRouteName="MemberDashboard"
+      screenOptions={{ headerShown: false }}
+    >
+      <HomeStackNavigator.Screen
+        name="MemberDashboard"
+        component={MemberDashboard}
+      />
+      <HomeStackNavigator.Screen name="Notification" component={Notification} />
+      <HomeStackNavigator.Screen name="ProfileStack" component={ProfileStack} />
+      <HomeStackNavigator.Screen name="CheckIn" component={CheckIn} />
+      <HomeStackNavigator.Screen name="Classes" component={Classes} />
+    </HomeStackNavigator.Navigator>
+  );
 };
 
-export default HomeStack
+export default HomeStack;
