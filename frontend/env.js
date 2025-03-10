d6906723308051c8ae203ed55347a12c3a1f8456
D0GL0V3R <wanyinapu@gmail.com>
2025-03-11 00:53:47 +0800

    achievement and leaderboard backend done

--- frontend/env.js
+++ frontend/env.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 const API_BASE_URL = "http://172.20.10.2:3000";
-=======
-const API_BASE_URL = "http://192.168.0.18:3000";
->>>>>>> 6a4918c38c70af2cf5262663b482960af8786f46
 
 export default API_BASE_URL;
